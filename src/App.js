3e35ac91e09cf11d2fb982e41f28f8affe33c695
Isaque Franklin <isaquefrankli@gmail.com>
2021-09-08 18:48:08 -0300

    login functionality done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
+import firebase, { FirebaseContext } from './firebase'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import useAuth from './authentication/useAuth'
 import Login from './authentication/Login'
+import ForgotPassword from './authentication/ForgotPassword'
+import Home from './components/Home'
 
 function App() {
+
+  const user = useAuth()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TwitterBr</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <FirebaseContext.Provider value={{ user, firebase}}>
+        <div className="app-container">
+          <div className="route-container">
+            <Switch>
+              <Route exact path='/' render={() => <Redirect to='/home' />} />
+              <Route path='/home' component={Home} />
+              <Route path='/login' component={Login} />
+              <Route path='/forgotpassword' component={ForgotPassword} />
+            </Switch>
+          </div>
+        </div>
+      </FirebaseContext.Provider>
+    </BrowserRouter>
   );
 }
 
--- src/authentication/Login.js
+++ src/authentication/Login.js
@@ -3,8 +3,8 @@ import useFormValidation from './useFormValidation'
 import validateLogin from './validateLogin'
 import { Link } from 'react-router-dom'
 import firebase from '../firebase/firebase'
-import { Form, Card } from 'react-bootstrap'
-
+import { Form, Container } from 'react-bootstrap'
+import CenteredContainer from './CenteredContainer'
 
 const INITIAL_STATE = {
     name: "",
@@ -34,11 +34,80 @@ function Login(props) {
     }
 
 
-    
+
     return (
-        <div>
-            
-        </div>
+        <CenteredContainer>
+            <h2 className="titulo">{login ? "Login" : "Crie sua conta"}</h2>
+            <Form onSubmit={handleSubmit} className="flex flex-column">
+                {!login && (
+                    <>
+                    <Form.Group id="name">
+                        <Form.Label>Your name</Form.Label>
+                        <Form.Control
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.name}
+                        type="text"
+                        placeholder="your name"
+                        autoComplete="off"
+                        name="name"
+                        className="input" />
+                    </Form.Group>
+                    {errors.name && <p className="error-text">{errors.name}</p>}
+
+                    <Form.Group id="username">
+                        <Form.Label>your username <small>*no spaces*</small></Form.Label>
+                        <Form.Control
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.username}
+                        type="text"
+                        placeholder="Your username"
+                        autoComplete="off"
+                        name="username"
+                        className="input" />
+                    </Form.Group>
+                    </>
+                )}
+
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                        type="email"
+                        placeholder="Your email"
+                        autoComplete="off"
+                        name="email"
+                        className={errors.email ? 'error-input' : "input"} />
+                    </Form.Group>
+                    {errors.email && <p className="error-text">{errors.email}</p>}
+
+                    <Form.Group id="password">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                        type="password"
+                        placeholder="chose a secure password"
+                        name="password"
+                        className={errors.password ? "error.input" : "input"} />
+                    </Form.Group>
+                    {errors.password && <p className="error-text">{errors.password}</p>}
+                    {firebaseError && <p className='error-text'>{firebaseError}</p>}
+                    <button type="submit" className="" disbaled={isSubmitting}>
+                      {login ? "Login" : "Create account"}
+                    </button>
+                    <button type="button" className="" onClick={() => setLogin(prevLogin => !prevLogin)}>
+                        {login ? "Ainda não tem uma conta?" : "Já tem uma conta?"}
+                    </button>
+            </Form>
+            <div className="w-100 text-left mt-3">
+              <Link to="/forgot">Esqueci minha senha</Link>
+            </div>
+        </CenteredContainer>
     )
 }
 
